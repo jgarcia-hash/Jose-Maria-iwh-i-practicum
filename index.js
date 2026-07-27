1b7ca5b722acc916613b6ebf02abc9ce348564ab
jgarcia-hash <jgarcia@paystand.com>
2026-07-27 10:43:32 -0600

    Created route 1 and route 2

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,15 +10,53 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const custom_obj = 'https://api.hubapi.com/crm/v3/objects/2-66508491?properties=name,age,specie';
+
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(custom_obj, { headers });
+        const data = resp.data.results;
+        
+        res.render('homepage', {title: 'Custom Objects', data});
+
+    } catch (error) {
+        console.error(error);
+    }
+})
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+    const name = req.query.name;
+    const getPet = `https://api.hubapi.com/crm/objects/2026-03/2-66508491/${name}?idProperty=name&properties=name,age,specie`
+
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+
+    try {
+        const resp = await axios.get(getPet, { headers });
+        const data = resp.data;
+        res.render('updates', {
+            title: 'Update Custom Object Form | Integrating With HubSpot I Practicum',
+            name: data.properties.name, 
+            specie: data.properties.specie, 
+            age: data.properties.age
+        });
+    } catch (error) {
+        console.error(error);
+        
+    }
+})
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
